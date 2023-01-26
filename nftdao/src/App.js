a25241c78d93597bbb7c27ccad2d68242303695c
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2023-01-26 19:28:45 +0530

    3d background

--- nftdao/src/App.js
+++ nftdao/src/App.js
@@ -1,34 +1,40 @@
 import "./App.css";
+import ModelViewer from "./components/threeD/Modelrenderer";
 import Home from "./components/pages/Home";
-import Createpage from './components/pages/Createpage';
-import Treasurypage from './components/pages/Treasurypage';
-import Aboutpage from './components/pages/Aboutpage';
+import Createpage from "./components/pages/Createpage";
+import Treasurypage from "./components/pages/Treasurypage";
+import Aboutpage from "./components/pages/Aboutpage";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Nftpage from './components/pages/Nftpage';
-import NoPageFound from './components/pages/NoPageFound';
+import Nftpage from "./components/pages/Nftpage";
+import NoPageFound from "./components/pages/NoPageFound";
 import Delegatepage from "./components/pages/Delegatepage";
 import Proposal from "./components/pages/Proposal";
-import Registration from './components/pages/Registration';
+import Registration from "./components/pages/Registration";
 import Login from "./components/pages/Login";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/Home" component={Home} />
-          <Route path="/Login" component={Login} />
-          <Route exact path="/" component={Registration} />
-          <Route path="/Proposal" component={Proposal} />
-          <Route path="/Create" component={Createpage} />
-          <Route path="/Treasury" component={Treasurypage} />
-          <Route path="/About" element={<Aboutpage />} />
-          {/* <Route path="/views/:id" element={<Detailspage />} /> */}
-          <Route path="/Nft" component={Nftpage} />
-          <Route path="/Delegate" element={<Delegatepage />} />
-          <Route path="*" element={<NoPageFound />}></Route>
-        </Switch>
-      </BrowserRouter>
+      <div id="threedcomp">
+        <ModelViewer scale="0.09" modelPath={"minecraft3.glb"} />
+      </div>
+      <div id="remainingapp">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/Home" component={Home} />
+            <Route path="/Login" component={Login} />
+            <Route exact path="/" component={Registration} />
+            <Route path="/Proposal" component={Proposal} />
+            <Route path="/Create" component={Createpage} />
+            <Route path="/Treasury" component={Treasurypage} />
+            <Route path="/About" element={<Aboutpage />} />
+            {/* <Route path="/views/:id" element={<Detailspage />} /> */}
+            <Route path="/Nft" component={Nftpage} />
+            <Route path="/Delegate" element={<Delegatepage />} />
+            <Route path="*" element={<NoPageFound />}></Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
